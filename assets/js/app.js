a78800f2d3c6bd1e63583f6e1366129032005a7b
Jeff Cole <jeffreydonaldcole@gmail.com>
2019-01-12 10:53:21 -0500

    chore(setup): remove webpack

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,7 @@
-// We need to import the CSS so that webpack will load it.
-// The MiniCssExtractPlugin is used to separate it out into
-// its own CSS file.
+// We need to import the CSS so that ??? will load it.
 import css from "../css/app.css"
 
-// webpack automatically bundles all modules in your
-// entry points. Those entry points can be configured
-// in "webpack.config.js".
+// ??? automatically bundles all modules in your entry points.
 //
 // Import dependencies
 //
